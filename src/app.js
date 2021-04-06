2bcd814b1c7a10131bfa36334d814fc160dd9d3a
Jordan <jordanace83@gmail.com>
2021-04-05 22:01:04 -0500

    React App Working

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return(
+    <div>
+      <h1>MVP</h1>
+      <h3>Constellations coming your way hella fast</h3>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
