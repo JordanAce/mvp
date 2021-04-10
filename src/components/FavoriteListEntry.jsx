296334aff97f44f1341b785b44640bf60e063c8e
Jordan <jordanace83@gmail.com>
2021-04-10 12:54:42 -0500

    MVP STARGAZER Complete

--- src/components/FavoriteListEntry.jsx
+++ src/components/FavoriteListEntry.jsx
@@ -6,8 +6,8 @@ const FavoriteListEntry = function(props) {
       <tbody>
         <tr>
           <td>Stardate: {props.favorite.date}</td>
-          <td>Title: {props.favorite.title}</td>
-          <td>Image: <a href = {props.favorite.url}>Click for Image!</a></td>
+          <td style = {{textAlign: 'center'}}>Title: {props.favorite.title}</td>
+          <td style = {{textAlign: 'right'}}>Image: <a href = {props.favorite.url}>Click for Image!</a></td>
         </tr>
       </tbody><br></br><br></br>
     </table>
